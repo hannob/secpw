83c7c5faee7f5dc0fb6ac85d0eb8a5f87439a1ad
Hanno Böck <990588+hannob@users.noreply.github.com>
2023-12-25 10:58:26 +0100

    js-beautify codingstyle with 2 spaces indent

--- secpw.js
+++ secpw.js
@@ -1,18 +1,18 @@
 function secpw(pwlen) {
-    if (pwlen === undefined) {
-        pwlen = 15;
-    }
+  if (pwlen === undefined) {
+    pwlen = 15;
+  }
 
-    const pwchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    const limit = 256 - (256 % pwchars.length);
+  const pwchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const limit = 256 - (256 % pwchars.length);
 
-    let passwd = "";
-    let randval;
-    for (let i = 0; i < pwlen; i++) {
-        do {
-            randval = window.crypto.getRandomValues(new Uint8Array(1))[0];
-        } while (randval >= limit);
-        passwd += pwchars[randval % pwchars.length];
-    }
-    return passwd;
+  let passwd = "";
+  let randval;
+  for (let i = 0; i < pwlen; i++) {
+    do {
+      randval = window.crypto.getRandomValues(new Uint8Array(1))[0];
+    } while (randval >= limit);
+    passwd += pwchars[randval % pwchars.length];
+  }
+  return passwd;
 }
