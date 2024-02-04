fd1516b08e06f49a8179637d5a79a97953b3dc99
Hanno Böck <990588+hannob@users.noreply.github.com>
2024-02-04 09:56:42 +0100

    allow custom character set

--- secpw.js
+++ secpw.js
@@ -1,12 +1,19 @@
 /* (c) Hanno Boeck, 0BSD license, https://password.hboeck.de/ */
-function secpw(pwlen) {
+function secpw(pwlen, pwchars) {
   if (pwlen === undefined) {
     pwlen = 15;
   } else if (!Number.isInteger(pwlen) || pwlen <= 0) {
     throw new Error("pwlen must be a positive integer");
   }
 
-  const pwchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  if (pwchars === undefined) {
+    const pwchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  } else if (typeof pwchars !== 'string') {
+    throw new Error("pwchars must be a string");
+  } else if (pwchars.length > 256) {
+    throw new Error("pwchars must be <= 256");
+  }
+
   const limit = 256 - (256 % pwchars.length);
 
   let passwd = "";
